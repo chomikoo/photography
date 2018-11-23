20eb325cbf9608a4f1ca8d710701b69ced20f49e
Szymon Trzepla <trzepla.szymon@gmail.com>
2018-11-23 10:40:28 +0100

    fix nav

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -8,18 +8,19 @@
  */
 ?>
 
+<nav>
+    <?php
+        wp_nav_menu( array( 
+            'theme_location' => 'top-menu', 
+            'container' => false,
+            // 'menu_class' => 'menu',
+            'items_wrap' => '<ul id="main-menu" class="menu">%3$s</ul>',
+            ) ); 
+            ?>
 
-<?php
-    wp_nav_menu( array( 
-        'theme_location' => 'top-menu', 
-        'container' => false,
-        // 'menu_class' => 'menu',
-        'items_wrap' => '<ul id="main-menu" class="menu">%3$s</ul>',
-        ) ); 
-?>
-
-<button id="hamburger" class="btn hamburger" aria-label="Menu Open">
-    <span class="hamburger__bar"></span>
-    <span class="hamburger__bar"></span>
-    <span class="hamburger__bar"></span>
-</button>
+    <button id="hamburger" class="btn hamburger" aria-label="Menu Open">
+        <span class="hamburger__bar"></span>
+        <span class="hamburger__bar"></span>
+        <span class="hamburger__bar"></span>
+    </button>
+</nav>
